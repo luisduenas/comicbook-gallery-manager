33d7d61591a7cdeee92f25ee5115732e01f20cad
luisduenas <luis.alonso.duenas.m@gmail.com>
2018-12-17 14:46:24 -0800

    update project

---
+++ src/ComicBookShared/Data/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookShared.Data
+{
+    public abstract class BaseRepository
+    {
+        protected Context Context { get; set; }
+        public BaseRepository(Context context)
+        {
+
+        }
+    }
+}
